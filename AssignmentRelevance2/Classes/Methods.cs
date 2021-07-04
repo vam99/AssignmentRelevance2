fd674f63f5ed094a7dfedd29dfd902f7b657bf9c
Vlad-Andrei MANOLE <vlad_andrei.manole@stud.acs.upb.ro>
2021-07-04 22:03:31 +0300

    commit1

---
+++ AssignmentRelevance2/Classes/Methods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AssignmentRelevance2.Classes
+{
+    public static class Methods
+    {
+        public static SqlConnection con = new SqlConnection();
+        public static SqlCommand com = new SqlCommand();
+        public static SqlDataReader dr;
+        public static void OpenCon(SqlConnection con)
+        {
+            //con.ConnectionString = "data source=.\\SQLEXPRESS; database=proiectRelevance; integrated security = SSPI;";
+            con.ConnectionString = "Server=DESKTOP-OMEOTO8\\VLAD; Database=proiectRelevance; Trusted_Connection=true;";
+            con.Open();
+        }
+        public static List<Produse> FetchProduse()
+        {
+            List<Produse> listaProduse = new List<Produse>();
+            if (listaProduse.Count > 0)
+            {
+                listaProduse.Clear();
+            }
+
+            try
+            {
+                Methods.OpenCon(con);
+                com.Connection = con;
+                com.CommandText = "SELECT Cod, Descriere, Pret, Cantitate FROM Produse";
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    listaProduse.Add(new Produse()
+                    {
+                        Cod = (int)dr["Cod"],
+                        Descriere = dr["Descriere"].ToString(),
+                        Pret = (decimal)dr["Pret"],
+                        Cantitate = (int)dr["Cantitate"],
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Eroare", ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return listaProduse;
+        }
+    }
+}
---
+++ AssignmentRelevance2/Classes/Produse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssignmentRelevance2
+{
+    public class Produse
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Campul Cod trebuie completat.")]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Campul Cod trebuie sa contina valori intregi mai mari ca 0.")]
+        public int? Cod { get; set; }
+
+        [Required(ErrorMessage = "Campul Descriere trebuie completat.")]
+        [MaxLength(10, ErrorMessage = "Campul Descriere trebuie sa aiba max 10 caractere.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Campul Descriere nu trebuie sa contina cifre.")]
+        public string Descriere { get; set; }
+
+        [Required(ErrorMessage = "Campul Pret trebuie completat.")]
+        [Range(0.01, 99999.99, ErrorMessage = "Pretul trebuie sa fie pozitiv si nu mai mare de 100000")]
+        [RegularExpression(@"^\d+\.\d{0,2}$", ErrorMessage = "Pretul trebuie sa aiba maxim 2 zecimale")]
+        public decimal? Pret { get; set; }
+
+        [Required(ErrorMessage = "Campul Cantitate trebuie completat.")]
+        [Range(0, Int32.MaxValue, ErrorMessage = "Campul Cantitate trebuie sa contina valori intregi mai mari ca 0.")]
+        public int? Cantitate { get; set; }
+
+        public decimal getValoare()
+        {
+            return (decimal)(this.Pret*this.Cantitate);
+        }
+    }
+}
